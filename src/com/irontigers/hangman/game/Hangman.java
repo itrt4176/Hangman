099449dda9abcb2b39e20777c681a26b7d51c69b
Nick Manos <nickpmanos@gmail.com>
2019-12-23 00:14:30 -0500

    Add javadoc to Hangman main()

--- src/com/irontigers/hangman/game/Hangman.java
+++ src/com/irontigers/hangman/game/Hangman.java
@@ -1,6 +1,10 @@
 package com.irontigers.hangman.game;
 
 public class Hangman {
+    /**
+     * This is the main entry point to the program.
+     * @param args  command-line arguments (we don't use this)
+     */
     public static void main(String[] args) {
         
     }
